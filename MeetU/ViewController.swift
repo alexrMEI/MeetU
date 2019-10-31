2d9ac20a9fd42e5ff22de18778a8b393fcfe4445
MestradoMEICM <2190275@my.ipleiria.pt>
2019-10-31 18:38:40 +0000

    Initial Commit

---
+++ MeetU/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeetU
+//
+//  Created by Ricardo Miguel da Silva Rodrigues on 31/10/2019.
+//  Copyright © 2019 MeetU Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
