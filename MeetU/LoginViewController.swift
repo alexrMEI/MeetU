1b43c0e486cfd4d5b6ec905a0dfb3e3ca21065c1
Alexandre Rodrigues <2190273@my.ipleiria.pt>
2019-11-22 18:35:16 +0000

    rename do view controller do login

--- MeetU/LoginViewController.swift
+++ MeetU/LoginViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 import Firebase
 
 @objc(EmailViewController)
-class EmailViewController: UIViewController {
+class LoginViewController: UIViewController {
     
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
