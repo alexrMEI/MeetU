bb1101e03483d78f0602876cbc924fe00a0eeb8a
FilipaFonte <f.fonte18@gmail.com>
2019-12-06 22:46:20 +0000

    Picker para imagem de perfil de utilizador

---
+++ MeetU/ProfileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileViewController.swift
+//  MeetU
+//
+//  Created by Filipa Reis da Fonte on 06/12/2019.
+//  Copyright © 2019 MeetU Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var profilePhoto: UIImageView!
+    
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController)
+    {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+    didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey :
+    Any]) {
+    // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage   else {
+                fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+            }
+        profilePhoto.image = selectedImage
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ MeetU/UserController.swift
@@ -0,0 +1,21 @@
+//
+//  UserController.swift
+//  MeetU
+//
+//  Created by Filipa Reis da Fonte on 06/12/2019.
+//  Copyright © 2019 MeetU Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserController {
+    
+    var name: String
+    var email: String
+    
+    init(name: String, email: String) {
+        self.name = name
+        self.email = email
+    }
+}
