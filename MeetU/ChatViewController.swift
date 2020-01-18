b52b89395595c544bb0f89a2d2e1f01c717b0d37
Ricardo Rodrigues <2190275@my.ipleiria.pt>
2020-01-18 18:04:05 +0000

    chat done for testing (correct bugs on login)

---
+++ MeetU/ChatViewController.swift
@@ -0,0 +1,196 @@
+//
+//  ChatViewController.swift
+//  MeetU
+//
+//  Created by Alexandre dos Santos Rodrigues on 18/01/2020.
+//  Copyright © 2020 MeetU Inc. All rights reserved.
+//
+
+import UIKit
+import InputBarAccessoryView
+import Firebase
+import MessageKit
+import FirebaseFirestore
+
+// (https://medium.com/@ibjects/simple-text-chat-app-using-firebase-in-swift-5-b9fa91730b6c)
+class ChatViewController: MessagesViewController, InputBarAccessoryViewDelegate, MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+    
+    var currentUser = Auth.auth().currentUser!
+    private var docReference: DocumentReference?
+    var messages: [Message] = []
+    //I've fetched the profile of user 2 in previous class from which //I'm navigating to chat view. So make sure you have the following //three variables information when you are on this class.
+    var user2Name: String?
+    var user2ImgUrl: String?
+    var user2UID: String?
+    
+    @IBOutlet weak var inputTxt: UITextField!
+    @IBOutlet weak var sendBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = user2Name ?? "Chat"
+
+        navigationItem.largeTitleDisplayMode = .never
+        maintainPositionOnKeyboardFrameChanged = true
+        //messageInputBar.inputTextView.tintColor = .primary
+        //messageInputBar.sendButton.setTitleColor(.primary, for: .normal)
+        
+        self.user2Name = "riky"
+        self.user2UID = "6AtznPqjviUKieCbLdLh4PqW4zi2"
+        
+        messageInputBar.delegate = self
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+        
+        loadChat()
+    }
+    
+    // MARK: LoadChat
+    //Create a new Chat if there is no chat available between users
+    //Load the chat if chat is available for users
+    func loadChat() {
+        //Fetch all the chats which has current user in it
+        let db = Firestore.firestore().collection("Chats").whereField("users", arrayContains: Auth.auth().currentUser?.uid ?? "Not Found User 1")
+        db.getDocuments { (chatQuerySnap, error) in
+            if let error = error {
+                print("Error: \(error)")
+                return
+            } else {
+                //Count the no. of documents returned
+                guard let queryCount = chatQuerySnap?.documents.count else {
+                    return
+                }
+                print("COUNT \(queryCount)")
+                if queryCount == 0 {
+                    //If documents count is zero that means there is no chat available and we need to create a new instance
+                    self.createNewChat()
+                }
+                else if queryCount >= 1 {
+                    //Chat(s) found for currentUser
+                    for doc in chatQuerySnap!.documents {
+                        let chat = Chat(dictionary: doc.data())
+                        //Get the chat which has user2 id
+                        if (chat?.users.contains(self.user2UID!))! {
+                            self.docReference = doc.reference
+                            //fetch it's thread collection
+                            doc.reference.collection("thread")
+                                .order(by: "created", descending: false)
+                                .addSnapshotListener(includeMetadataChanges: true, listener: { (threadQuery, error) in
+                                    if let error = error {
+                                        print("Error: \(error)")
+                                        return
+                                    } else {
+                                        self.messages.removeAll()
+                                        for message in threadQuery!.documents {
+                                            let msg = Message(dictionary: message.data())
+                                            self.messages.append(msg!)
+                                            print("Data: \(msg?.content ?? "No message found")")
+                                        }
+                                        self.messagesCollectionView.reloadData()
+                                        self.messagesCollectionView.scrollToBottom(animated: true)
+                                    }
+                                })
+                            return
+                        } //end of if
+                    } //end of for
+                    self.createNewChat()
+                } else {
+                    print("Let's hope this error never prints!")
+                }
+            }
+        }
+    }
+    
+    // MARK: Create New Chat
+    //chat is not available for a user
+    func createNewChat() {
+        let users = [self.currentUser.uid, self.user2UID]
+        let data: [String: Any] = [
+            "users":users
+        ]
+        let db = Firestore.firestore().collection("Chats")
+        db.addDocument(data: data) { (error) in
+            if let error = error {
+                print("Unable to create chat! \(error)")
+                return
+            } else {
+                self.loadChat()
+            }
+        }
+    }
+    
+    // MARK: Insert New Message
+    //insert a new message in the feed
+    private func insertNewMessage(_ message: Message) {
+        //add the message to the messages array and reload it
+        messages.append(message)
+        messagesCollectionView.reloadData()
+        DispatchQueue.main.async {
+            self.messagesCollectionView.scrollToBottom(animated: true)
+        }
+    }
+    
+    // MARK: Save Message
+    //save that message on firestore
+    private func save(_ message: Message) {
+        //Preparing the data as per our firestore collection
+        let data: [String: Any] = [
+            "content": message.content,
+            "created": message.created,
+            "id": message.id,
+            "senderID": message.senderID,
+            "senderName": message.senderName
+        ]
+        //Writing it to the thread using the saved document reference we saved in load chat function
+        docReference?.collection("thread").addDocument(data: data, completion: { (error) in
+            if let error = error {
+                print("Error Sending message: \(error)")
+                return
+            }
+            self.messagesCollectionView.scrollToBottom()
+        })
+    }
+    
+    // MARK: DELEGATE METHODS
+    
+    // MARK: InputBarAccessoryViewDelegate
+    func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
+        //When use press send button this method is called.
+        let message = Message(id: UUID().uuidString, content: text, created: Timestamp(), senderID: currentUser.uid, senderName: currentUser.email!)
+        //calling function to insert and save message
+        insertNewMessage(message)
+        save(message)
+        //clearing input field
+        inputBar.inputTextView.text = ""
+        //inputTxt.text = ""
+        messagesCollectionView.reloadData()
+        messagesCollectionView.scrollToBottom(animated: true)
+    }
+    
+    // MARK: MessagesDataSource
+    //This method return the current sender ID and name
+    func currentSender() -> SenderType {
+        return Sender(id: Auth.auth().currentUser!.uid, displayName: Auth.auth().currentUser?.displayName ?? "Name not found")
+    }
+    //This return the MessageType which we have defined to be text in Messages.swift
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+    }
+    //Return the total number of messages
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        if messages.count == 0 {
+            print("There are no messages")
+            return 0
+        } else {
+            return messages.count
+        }
+    }
+    
+    @IBAction func SendBtn(_ sender: UIButton) {
+
+    }
+    
+    
+}
